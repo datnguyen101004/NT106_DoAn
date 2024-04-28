52dbd69aa1ba7306392245b667cadbdef90152dc
Minotaurus1712 <duonghung171204sd@gmail.com.vn>
2024-04-29 00:02:52 +0700

    Hung:update UI Play

--- FE/DoAnNT106/Play.Designer.cs
+++ FE/DoAnNT106/Play.Designer.cs
@@ -28,15 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Play));
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.btnSendMessage = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.SuspendLayout();
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(2, 375);
+            this.richTextBox1.Location = new System.Drawing.Point(809, 374);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(450, 255);
             this.richTextBox1.TabIndex = 0;
@@ -45,7 +51,7 @@
             // richTextBox2
             // 
             this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox2.Location = new System.Drawing.Point(2, 627);
+            this.richTextBox2.Location = new System.Drawing.Point(809, 626);
             this.richTextBox2.Name = "richTextBox2";
             this.richTextBox2.Size = new System.Drawing.Size(359, 44);
             this.richTextBox2.TabIndex = 1;
@@ -53,7 +59,7 @@
             // 
             // btnSendMessage
             // 
-            this.btnSendMessage.Location = new System.Drawing.Point(358, 627);
+            this.btnSendMessage.Location = new System.Drawing.Point(1165, 626);
             this.btnSendMessage.Name = "btnSendMessage";
             this.btnSendMessage.Size = new System.Drawing.Size(94, 45);
             this.btnSendMessage.TabIndex = 2;
@@ -64,7 +70,7 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.Red;
-            this.button1.Location = new System.Drawing.Point(1072, 598);
+            this.button1.Location = new System.Drawing.Point(1118, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(132, 54);
             this.button1.TabIndex = 3;
@@ -72,17 +78,67 @@
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Image = global::DoAnNT106.Properties.Resources.Scissors;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(12, 597);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 63);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Kéo";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Image = global::DoAnNT106.Properties.Resources.ROCK;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(149, 597);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 63);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Búa";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Image = global::DoAnNT106.Properties.Resources.bao;
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.Location = new System.Drawing.Point(286, 598);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(118, 63);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Bao";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "ROCK.png");
+            this.imageList1.Images.SetKeyName(1, "Scissors.png");
+            this.imageList1.Images.SetKeyName(2, "bao.png");
+            // 
             // Play
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnSendMessage);
             this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.richTextBox1);
             this.Name = "Play";
-            this.Text = "HomePage";
+            this.Text = "git ";
             this.ResumeLayout(false);
 
         }
@@ -93,5 +149,9 @@
         private System.Windows.Forms.RichTextBox richTextBox2;
         private System.Windows.Forms.Button btnSendMessage;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ImageList imageList1;
     }
 }
