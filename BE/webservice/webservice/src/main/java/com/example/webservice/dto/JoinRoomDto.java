0bdfc8bfc31fa296d676cead75e309f99d619cb3
Đạt <datnguyen101004@gmail.com>
2024-05-09 11:46:24 +0700

    update BE

--- BE/webservice/webservice/src/main/java/com/example/webservice/dto/JoinRoomDto.java
+++ BE/webservice/webservice/src/main/java/com/example/webservice/dto/JoinRoomDto.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserDto {
+public class JoinRoomDto {
     private String username;
     private String roomId;
 }
