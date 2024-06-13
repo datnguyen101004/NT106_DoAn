3464a02566889337acd494f4b7f50c4f0d81e258
Minotaurus1712 <duonghung171204sd@gmail.com.vn>
2024-06-13 10:29:06 +0700

    Update Info

--- FE/DoAnNT106/EditInfo.Designer.cs
+++ FE/DoAnNT106/EditInfo.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DoAnNT106
 {
-    partial class Info
+    partial class EditInfo
     {
         /// <summary>
         /// Required designer variable.
@@ -34,6 +34,7 @@
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,9 +44,8 @@
             this.textBox1.Location = new System.Drawing.Point(592, 143);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(181, 32);
-            this.textBox1.TabIndex = 6;
+            this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
@@ -53,7 +53,6 @@
             this.textBox2.Location = new System.Drawing.Point(592, 266);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.ReadOnly = true;
             this.textBox2.Size = new System.Drawing.Size(181, 32);
             this.textBox2.TabIndex = 1;
             // 
@@ -63,7 +62,6 @@
             this.textBox3.Location = new System.Drawing.Point(592, 368);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
-            this.textBox3.ReadOnly = true;
             this.textBox3.Size = new System.Drawing.Size(386, 32);
             this.textBox3.TabIndex = 2;
             // 
@@ -73,36 +71,48 @@
             this.textBox4.Location = new System.Drawing.Point(971, 143);
             this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
-            this.textBox4.ReadOnly = true;
             this.textBox4.Size = new System.Drawing.Size(125, 32);
             this.textBox4.TabIndex = 3;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(96, 116);
+            this.pictureBox1.Location = new System.Drawing.Point(110, 82);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(173, 201);
+            this.pictureBox1.Size = new System.Drawing.Size(195, 203);
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Segoe UI Variable Text", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(1130, 40);
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(151, 300);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(95, 46);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Sửa";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Size = new System.Drawing.Size(111, 39);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Thêm ảnh";
+            this.button1.UseVisualStyleBackColor = false;
             // 
-            // Info
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI Variable Text", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(751, 449);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 45);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Lưu";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // EditInfo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::DoAnNT106.Properties.Resources.info;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.textBox4);
@@ -110,7 +120,7 @@
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.DoubleBuffered = true;
-            this.Name = "Info";
+            this.Name = "EditInfo";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Info";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -127,5 +137,6 @@
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
